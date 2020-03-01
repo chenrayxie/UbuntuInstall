eedaad116ff9f6b1dc3eda764c506f3c659f1aa9
chenrayxie <u06157048@o365.mcut.edu.tw>
2020-03-01 23:30:49 +0800

    first commit

---
+++ InstallBasicApp.sh
@@ -0,0 +1,26 @@
+echo "google chrome"
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
+
+echo "snap"
+sudo apt install update
+sudo apt install snap
+
+echo "android-studio"
+sudo apt install openjdk-8-jre openjdk-8-jdk
+sudo snap download "android-studio"
+sudo snap install android-studio_83.snap
+
+echo "pycharm"
+sudo snap install pycharm-community --classic
+
+echo "docker"
+sudo apt install docker.io
+
+echo "github"
+sudo apt update
+sudo apt upgrade
+sudo apt install git
+
+
